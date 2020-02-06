7692f2dac0ba5f96df9b96ab0ecf2ea5727ffb9b
Sven Seeberg <mail@sven-seeberg.de>
2020-02-06 15:53:02 +0100

    Add CORS header

--- ig-cache-config.sh
+++ ig-cache-config.sh
@@ -3,6 +3,10 @@ CONFPATH="/etc/apache2/conf-available/ig-aliases.conf"
 rm $CONFPATH
 touch $CONFPATH
 echo "AddType application/json .json" >> $CONFPATH
+echo "<LocationMatch ^(.*)/wp-json/(.*)$>" >> $CONFPATH
+echo "        Header set Access-Control-Allow-Origin *" >> $CONFPATH
+echo "</LocationMatch>" >> $CONFPATH
+
 while IFS= read -r line
 do
   mkdir -p "/var/www/ig-cache/$(dirname $line)"
